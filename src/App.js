caa92753f37a400adac57805525386aca5b89e75
keepdying <yunusemre34tr@gmail.com>
2021-09-15 18:34:46 +0300

    Add update time

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import players_json from './players.json'
 import currentRuns from './runs.json'
 import {useState} from 'react'
 import {PlayerAccordion} from './utils';
-//import lastupdated from './lastUpdated.json'
+import lastupdated from './lastUpdated.json'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
 
   return (
     <div className="App">
-      <div className="app-header">Firestorm RIO (WIP!)</div>
-      <p>Made by keepdying#5821. Last updated on ---</p>
+      <div className="app-header">Firestorm RIO (Beta!)</div>
+      <p>Made by keepdying#5821. Last updated on {lastupdated}</p>
       <p>Total Players: {players_json.length}, Total Runs: {currentRuns.length}</p>
       <p> F.A.Q: <a href="https://github.com/keepdying/firestorm-rio">https://github.com/keepdying/firestorm-rio</a></p>
 
