249c6aabc812b3bd628df56632a8b46239acc309
keepdying <yunusemre34tr@gmail.com>
2021-09-19 18:49:30 +0300

    Add class selector

--- src/screens/playerListScreen/index.jsx
+++ src/screens/playerListScreen/index.jsx
@@ -6,10 +6,26 @@ import PlayerAccordion from '../../components/playerAccordion';
 
 import players_json from '../../players.json'
 import currentRuns from '../../runs.json';
+import pclasses from '../../pclasses.json';
 
 import * as styles from "./styles.module.scss";
 
+import Select from "react-select"
+
 const playersPerPage = 25;
+const selectorStyle = {
+    control: (base) => ({
+        ...base,
+        scale: "75%",
+        marginTop: 0,
+        width: 250
+
+    }),
+    input: (base) => ({
+        ...base,
+        margin: '0px',
+      }),
+  };
 
 export default class PlayerListScreen extends React.Component {
     constructor(props){
@@ -19,8 +35,10 @@ export default class PlayerListScreen extends React.Component {
             searchTerm: "",
             allPlayers: [],
             filteredPlayers: [],
+            selectedPlayers: [],
             visiblePlayers: [],
-            page: 0
+            page: 0,
+            selectedOption: null,
         }
         
     }
@@ -31,7 +49,7 @@ export default class PlayerListScreen extends React.Component {
             player.position = index + 1
         })
 
-        this.setState({ allPlayers: initialPlayers, filteredPlayers: initialPlayers, visiblePlayers: initialPlayers.slice(0, playersPerPage) });
+        this.setState({ allPlayers: initialPlayers, selectedPlayers: initialPlayers, filteredPlayers: initialPlayers, visiblePlayers: initialPlayers.slice(0, playersPerPage)});
     }
 
     handlePageClick = (data) => {
@@ -39,25 +57,57 @@ export default class PlayerListScreen extends React.Component {
         this.setState({ visiblePlayers: this.state.filteredPlayers.slice(page * playersPerPage, (page + 1) * playersPerPage), page })
     };
 
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        let { page, allPlayers} = this.state;
+
+        if (selectedOption['value'] === "noclass"){
+            page = 0;
+            allPlayers.forEach((player, index) => {
+                player.position = index + 1
+            })
+            this.setState({ selectedPlayers: allPlayers, filteredPlayers: allPlayers, visiblePlayers: allPlayers.slice(page * playersPerPage, (page + 1) * playersPerPage), page: 0 }); 
+        } else {
+            page = 0;
+            let selectedPlayers = allPlayers.filter(player => player.pclass === selectedOption['value']);
+            selectedPlayers.forEach((player, index) => {
+                player.position = index + 1
+            })
+            this.setState({ selectedPlayers: selectedPlayers, filteredPlayers: selectedPlayers, visiblePlayers: selectedPlayers.slice(page * playersPerPage, (page + 1) * playersPerPage), page: 0 });
+        }
+        // console.log(`Option selected:`, selectedOption);
+      };
+
     updateFilteredPlayers = (event) => {
-        let { page, allPlayers } = this.state;
+        let { page, selectedPlayers } = this.state;
         let searchTerm = event.target.value;
-        let filteredPlayers = allPlayers.filter(player => player.name.toLowerCase().includes(searchTerm.toLowerCase()));
-        this.setState({ searchTerm, filteredPlayers, visiblePlayers: filteredPlayers.slice(page * playersPerPage, (page + 1) * playersPerPage), page: 0 });
+        let filteredPlayers = selectedPlayers.filter(player => player.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        this.setState({ searchTerm, filteredPlayers: filteredPlayers, visiblePlayers: filteredPlayers.slice(page * playersPerPage, (page + 1) * playersPerPage), page: 0 });
     }
     
 
     render(){
-        const { state, handlePageClick } = this;
-        const { visiblePlayers, filteredPlayers, page } = state; 
+        const { state, handlePageClick, handleChange } = this;
+        const { visiblePlayers, filteredPlayers, page, selectedOption } = state;
 
         return (
             <Container component="main" maxWidth="md" className={styles.playerListScreen}>
-                <input
-                    type="text" 
-                    placeholder="Search by name..." 
-                    onChange={(event) => this.updateFilteredPlayers(event)}
-                />
+                <div className={styles.topBars}>
+                    <input style={{ display: 'inline-block' }}
+                        type="text" 
+                        placeholder="Search by name..." 
+                        onChange={(event) => this.updateFilteredPlayers(event)}
+                    />
+                    <div className={styles.pclassSelector}>
+                    <Select
+                        value={selectedOption}
+                        onChange={handleChange}
+                        options={pclasses}
+                        styles={selectorStyle}
+                        defaultValue={pclasses[0]}
+                    />
+                    </div>
+                </div>
                 <div className={styles.playerList}>
                     <div className={styles.listHeaders}>
                         <div className={styles.listHeader}>
